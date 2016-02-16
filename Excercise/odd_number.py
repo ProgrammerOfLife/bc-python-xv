2f3afa13cb1ab2144455bc8c394950b163108a1d
Mayowa <Mayowa>
2016-02-16 13:12:56 +0100

    New Changes

---
+++ Excercise/odd_number.py
@@ -0,0 +1,9 @@
+def odd_numbers(low,high):
+    if low > high:
+        return "Invalid input"
+    else:
+        new_list = []
+	for i in range(low,high):
+            if i % 2 != 0:
+                new_list.append(i)
+    return new_list
